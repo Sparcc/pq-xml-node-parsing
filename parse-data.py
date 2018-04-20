3c12a291ff96c466a2d441b0830b368cf8a50daa
avih8r <sparco.209@gmail.com>
2018-04-20 10:59:06 +1000

    way too complicated :/

--- parse-data.py
+++ parse-data.py
@@ -3,39 +3,6 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('Organisational Structure As At Date.yxwz')
 root = tree.getroot()
 
-#nodes = root.findall(".//*[@ToolID='1']")
-#nodes = root.findall(".//Node")
-#allConnections = connections = root.findall(".//*Connection/")
-#i = 1
-#for c in allConnections:
-#    connections = root.findall(".//Connection/*[@ToolID='"+str(i)+"']")
-#    i+=1
-#conOrig = root.findall(".//*Connection/Origin")
-#conDest = root.findall(".//*Connection/Destination")
-#cons = root.findall(".//Connections/Connection")
-
-
-'''
-node = {}
-for o in root.iter('Connection'):
-    con = list(o)
-    origin=con[0]
-    destination=con[1]
-    currentNodeString = str(origin.get('ToolID'))
-    if currentNodeString in nodesDone:
-        #amend existing node
-        node[currentNodeString]['destination'] += ", " +str(destination.get('ToolID'))
-    else:
-        #build new node
-        node[currentNodeString] = {}
-        node[currentNodeString]['origin'] = currentNodeString
-        node[currentNodeString]['destination'] = str(destination.get('ToolID'))
-        nodesDone.append(currentNodeString)
-'''
-#annotation = root.findall(".//Node/[@ToolID='1']/Properties/Annotation/DefaultAnnotationText")
-#print(annotation[0].text)
-    
-
 class node:
     origin = ""
     destination = ""
@@ -51,32 +18,28 @@ class nodeParser:
         con = list(o)
         origin=str(con[0].get('ToolID'))
         destination=str(con[1].get('ToolID'))
-        currentNodeString = str(origin.get('ToolID'))
-        
-        self.paseNode()
-        if currentNodeString in self.nodesDone:
-            self.updateNode()
+        currentNode = str(origin.get('ToolID'))
+        if currentNode in self.nodesDone:
+            self.updateNode(currentNode, destination)
         else:
-            self.newNode()
-        
-    def parseNode(self, 
-        
-    def newNode(self):
+            self.newNode(currentNode, destination)
+            
+    def newNode(self, currentNode, destination):
         #build new node
-        self.nodes[currentNodeString] = node()
-        self.nodes[currentNodeString].origin = currentNodeString
-        self.nodes[currentNodeString].destination = destination
+        self.nodes[currentNode] = node()
+        self.nodes[currentNode].origin = currentNode
+        self.nodes[currentNode].destination = []
+        self.nodes[currentNode].destination.append(destination)
+        
+        for o in root.iter('Connection'):
+            self.parseConnection(o)
+        
         annotation = root.findall(".//Node/[@ToolID='1']/Properties/Annotation/DefaultAnnotationText")
-        self.nodes[currentNodeString].annotation = annotation[0].text
-        self.nodesDone.append(currentNodeString)
-    def updateNode(self):
+        self.nodes[currentNode].annotation = annotation[0].text
+        self.nodesDone.append(currentNode)
+    def updateNode(self, currentNode, destination):
         #amend existing node
-        self.nodes[currentNodeString].destination += ", " + destination
-
-
-    
-#print(nodesDone)
-
+        self.nodes[currentNode].destination.destination.append(destination)
 
 for n, v in nodes.items():
     print("ORIGIN NODE: "+v.origin)
