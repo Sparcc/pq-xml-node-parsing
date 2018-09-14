9c989525e3cf061b70a29d044eea67218d0455f2
Tom <sparco.209@gmail.com>
2018-09-14 20:46:23 +1000

    code cleanup

--- parse-data.py
+++ parse-data.py
@@ -3,38 +3,7 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('Organisational Structure As At Date.yxwz')
 root = tree.getroot()
 
-#nodes = root.findall(".//*[@ToolID='1']")
-#nodes = root.findall(".//Node")
-#allConnections = connections = root.findall(".//*Connection/")
-#i = 1
-#for c in allConnections:
-#    connections = root.findall(".//Connection/*[@ToolID='"+str(i)+"']")
-#    i+=1
-#conOrig = root.findall(".//*Connection/Origin")
-#conDest = root.findall(".//*Connection/Destination")
-#cons = root.findall(".//Connections/Connection")
-
 nodesDone = []
-'''
-node = {}
-for o in root.iter('Connection'):
-    con = list(o)
-    origin=con[0]
-    destination=con[1]
-    currentNodeString = str(origin.get('ToolID'))
-    if currentNodeString in nodesDone:
-        #amend existing node
-        node[currentNodeString]['destination'] += ", " +str(destination.get('ToolID'))
-    else:
-        #build new node
-        node[currentNodeString] = {}
-        node[currentNodeString]['origin'] = currentNodeString
-        node[currentNodeString]['destination'] = str(destination.get('ToolID'))
-        nodesDone.append(currentNodeString)
-'''
-#annotation = root.findall(".//Node/[@ToolID='1']/Properties/Annotation/DefaultAnnotationText")
-#print(annotation[0].text)
-    
 
 class node:
     origin = ""
@@ -58,10 +27,8 @@ for o in root.iter('Connection'):
         annotation = root.findall(".//Node/[@ToolID='"+currentNodeString+"']/Properties/Annotation/DefaultAnnotationText")
         nodes[currentNodeString].annotation = annotation[0].text
         nodesDone.append(currentNodeString)
-
-#print(nodesDone)
-
-
+        
+#print tree of nodes
 for n, v in nodes.items():
     print("ORIGIN NODE: "+v.origin)
     try:
